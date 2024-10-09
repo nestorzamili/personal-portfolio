642800eb9512890f4ff9ab61e7c52f7e98c6c3b8
Nestor <nestorzamili@gmail.com>
2024-10-09 22:23:01 +0700

    Refactor: Update image source in intro component and add Google site verification file

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,6 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
+import potrait from "@/public/potrait.jpg";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
@@ -31,7 +32,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="/potrait.jpg"
+              src={potrait}
               alt="Nestor potrait"
               width="192"
               height="192"
